bbca2edd93df696403d0ee372a661211a07df459
prisnak <prisnak@gmail.com>
2020-02-19 14:58:16 -0300

    updating files

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// document.getElementsByClassName("heroDesktop").style.display = "none";
+
+// function heroMobile (){
+//     var img = document.getElementsByClassName("heroDesktop");
+//         if (x.style.display === "none") {
+//             x.style.display = "block";
+//         } else {
+//             x.style.display = "none";
+//     }
+// }
